259925d29068f17a191a20a40729781f509325d6
MattyCho <myc.chosen@gmail.com>
2021-10-05 16:13:31 -0700

    added postgresql database support and get/post method for albums

--- src/main/java/com/springLab/songr/controllers/albumController.java
+++ src/main/java/com/springLab/songr/controllers/albumController.java
@@ -1,14 +1,22 @@
 package com.springLab.songr.controllers;
 
 import com.springLab.songr.models.Album;
+import com.springLab.songr.repositories.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class albumController {
+
+    @Autowired
+    AlbumRepository albumRepository;
 
     @GetMapping("/")
     public String homePage() {
@@ -20,11 +28,21 @@ public class albumController {
         Album frankSinatra = new Album("Come Fly With Me", "Frank Sinatra", 12, 2327, "/images/frankAlbumCover.jpg");
         Album nerd = new Album("In Search Of...", "N.E.R.D", 12, 3237, "/images/nerdAlbumCover.jpg");
         Album hamilton = new Album("Hamilton (Original Broadway Cast Recording)", "Lin-Manuel Miranda", 46, 8520, "/images/hamiltonAlbumCover.jpg");
-        ArrayList<Album> albums = new ArrayList<>();
-        albums.add(frankSinatra);
-        albums.add(nerd);
-        albums.add(hamilton);
+
+//        ArrayList<Album> albums = new ArrayList<>();
+//        albums.add(frankSinatra);
+//        albums.add(nerd);
+//        albums.add(hamilton);
+
+        List<Album> albums = albumRepository.findAll();
         m.addAttribute("albums", albums);
         return "album";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addNewAlbum(String title, String artist, int songCount, int length, String imageUrl) {
+        Album newAlbum = new Album(title, artist, songCount, length, imageUrl);
+        albumRepository.save(newAlbum);
+        return new RedirectView("albums");
     }
 }
